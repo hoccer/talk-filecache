2c603a71f2dc9a05275244d03dfa27f58900c410
Ingo Albrecht <prom@berlin.ccc.de>
2013-05-27 23:07:48 +0200

    Allow downloading NEW files

--- src/main/java/com/hoccer/talk/filecache/DownloadServlet.java
+++ src/main/java/com/hoccer/talk/filecache/DownloadServlet.java
@@ -157,8 +157,7 @@ public class DownloadServlet extends HttpServlet {
         // err if file is gone
         int fileState = file.getState();
         if(fileState == CacheFile.STATE_EXPIRED
-                || fileState == CacheFile.STATE_ABANDONED
-                || fileState == CacheFile.STATE_NEW) {
+                || fileState == CacheFile.STATE_ABANDONED) {
             resp.sendError(HttpServletResponse.SC_NOT_FOUND,
                     "File does not exist");
             return null;
